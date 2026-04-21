d34b9365e2f90486f49f4642eba6dbb2c35b76a6
yddfhbh <yddfhbh@gmail.com>
2026-04-21 20:46:48 +0900

    site update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const apiProxyTarget = process.env.API_PROXY_TARGET || "http://localhost:3000";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: apiProxyTarget,
         changeOrigin: true,
       },
     },
   },
-})
+});
