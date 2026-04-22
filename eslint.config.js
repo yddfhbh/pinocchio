3b205aa68708b48cb2cc8aa4c97a30854d736ff1
yddfhbh <yddfhbh@gmail.com>
2026-04-22 17:04:59 +0900

    site update

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,29 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build', '.svelte-kit']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [js.configs.recommended],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: {
         ...globals.browser,
         ...globals.node,
+        $bindable: 'readonly',
+        $derived: 'readonly',
+        $effect: 'readonly',
+        $host: 'readonly',
+        $inspect: 'readonly',
+        $props: 'readonly',
+        $state: 'readonly',
       },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      sourceType: 'module',
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^[A-Z_]' }],
     },
   },
 ])
